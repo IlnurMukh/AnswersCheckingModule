91f82a38a05c88f90a1d6f08212698fcdacfd99c
IlnurMukh <ilnur3793@gmail.com>
2024-07-06 18:46:29 +0300

    Добавьте файлы проекта.

---
+++ AnswersCheckingModule/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnswersCheckingModule.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
